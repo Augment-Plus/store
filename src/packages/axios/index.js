881b0de1e7607926070125a46685f0c13fe3be79
Augment Chinedu <augmentchinedu@gmail.com>
2025-08-09 09:15:53 +0100

    Packages Made

--- src/packages/axios/index.js
+++ src/packages/axios/index.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const isProd = process.env.NODE_ENV === "production";
+
+// Configure Axios instance
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
+  baseURL: isProd
+    ? `https://${window.location.hostname}/api`
+    : `${import.meta.env.VITE_API_BASE_URL}/api`,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
